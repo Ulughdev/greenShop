277a76e1e10e0505f0198639ff7b80a8fd1d6ee2
Ulughdev <ulugh0618@gmail.com>
2025-07-23 06:13:10 -0700

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 //import moment from "moment"; //const moment = require("moment")
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("ERORR on connection MongoDB", err));
